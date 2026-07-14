b23da0b700beccd4c17cd507c6cd68315a08d7fc
Abdalkrem2 <abdalkremalshbol8@gmail.com>
2026-07-14 15:35:42 +0300

    updates

--- json/2/script.js
+++ json/2/script.js
@@ -1,10 +1,21 @@
-import {jsonObject} from "./data.js"
-let table=document.getElementById("table");
-console.log(jsonObject)
-let data=JSON.parse(jsonObject)
-let arr=data.users;
-arr.forEach((element)=>{
-table.innerHTML+=`
+import { jsonObject } from "./data.js"
+let table = document.getElementById("table");
+// console.log(jsonObject)
+
+const button = document.getElementById("b")
+button.addEventListener("click", function () {
+    console.log("click");
+    table.style.display = "block"
+});
+
+button.addEventListener("dblclick", function () {
+    table.style.display = "none"
+});
+
+let data = JSON.parse(jsonObject)
+let arr = data.users;
+arr.forEach((element) => {
+    table.innerHTML += `
 <tr>
 <td>${element.firstName}</td>
 <td>${element.phone}</td>
